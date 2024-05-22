f9661e7b84a60222ab9b32b483ec84e3940aab88
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-22 21:25:44 +0800

    Create Sound.lua

---
+++ Sound.lua
@@ -0,0 +1,135 @@
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
+
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://9113731836"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
+wait(14)
